ae7523e0209ef798abd066bd4bec14dd43b6fc7e
sujoyDU <sujoydebnath.92@gmail.com>
2020-04-24 12:32:07 -0400

    webgis proj2

--- main.js
+++ main.js
@@ -1,28 +1,47 @@
 var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 2,
-          center: new google.maps.LatLng(2.8,-187.3),
-          mapTypeId: 'terrain'
-        });
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: new google.maps.LatLng(40.65,-73.72),
+    mapTypeId: 'roadmap'
+  });
+  
+  map.data.loadGeoJson('comdis.geojson');
+  map.data.setStyle({
+    fillColor: 'blue',
+    strokeWeight: 1
+  });
 
-        // Create a <script> tag and set the USGS URL as the source.
-        var script = document.createElement('script');
-        // This example uses a local copy of the GeoJSON stored at
-        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-        script.src = 'earthquake_GeoJSONP.js';
-        document.getElementsByTagName('head')[0].appendChild(script);
-      }
+  // map.data.addListener('mouseover', function(event) {
+  //   document.getElementById('info-box').textContent =
+  //       event.feature.getProperty('boro_cd');
+  // });
+  map.data.addListener('mouseover', mouseInToRegion);
+  map.data.addListener('mouseout', mouseOutOfRegion);
+}
+function mouseInToRegion(e) {
+  // set the hover state so the setStyle function can change the border
+  e.feature.setProperty('state', 'hover');
 
-      // Loop through the results array and place a marker for each
-      // set of coordinates.
-      window.eqfeed_callback = function(results) {
-        for (var i = 0; i < results.features.length; i++) {
-          var coords = results.features[i].geometry.coordinates;
-          var latLng = new google.maps.LatLng(coords[1],coords[0]);
-          var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-          });
-        }
-      }
+  // var percent = (e.feature.getProperty('census_variable') - censusMin) /
+  //     (censusMax - censusMin) * 100;
+
+  // update the label
+  document.getElementById('data-label').textContent =
+      e.feature.getProperty('boro_cd');
+  document.getElementById('data-value').textContent =
+      e.feature.getProperty('shape_area').toLocaleString();
+  document.getElementById('data-box').style.display = 'block';
+  //document.getElementById('data-caret').style.display = 'block';
+  // document.getElementById('data-caret').style.paddingLeft = percent + '%';
+}
+
+/**
+ * Responds to the mouse-out event on a map shape (state).
+ *
+ * @param {?google.maps.MouseEvent} e
+ */
+function mouseOutOfRegion(e) {
+  // reset the hover state, returning the border to normal
+  e.feature.setProperty('state', 'normal');
+}
